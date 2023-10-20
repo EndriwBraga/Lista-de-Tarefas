76c5f786d362400dee42b72de0050b2114f2b9b1
Endriw Braga <endriwbragalisboa@gmail.com>
2023-10-20 11:22:29 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const inputContent = document.querySelector('.input-container');
+const btnSave = document.querySelector('.button-container');
+const tasksContent = document.querySelector('.tasks')
+
+
+
+btnSave.addEventListener('click', addTask);
+
+function addTask() {
+    const inputValue = inputContent.value;
+
+
+}
+addTask()
