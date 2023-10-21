99f1f70575acc4f599b4e2e5fde908a1e464c28f
Endriw Braga <endriwbragalisboa@gmail.com>
2023-10-21 02:13:27 -0300

    feat(task): Criei a função que cria um 'li' e adiciona a tarefa nele visivel para o usuario

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ const tasksContent = document.querySelector('.tasks')
 
 btnSave.addEventListener('click', addTask);
 
-function addTask() {
+function addTask(event) {
     const inputValue = inputContent.value;
 
+    event.preventDefault();
+    if(inputValue === '') {
+        alert('Preencha uma tarefa por favor!')
+    } else{
+   
+   
+    const newContent = document.createElement('li');
+    newContent.textContent = inputValue;
 
+    tasksContent.appendChild(newContent)
+    console.log(inputValue);
 }
-addTask()
+}
+
