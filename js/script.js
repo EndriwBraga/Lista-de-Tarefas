c43617116f1da361027e1d399f13805d3bd654fb
Endriw Braga <endriwbragalisboa@gmail.com>
2023-10-21 04:54:25 -0300

    feat(task): Adicionar mensagens de erro e alterar cor do input em caso de erro

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
-const inputContent = document.querySelector('.input-container');
-const btnSave = document.querySelector('.button-container');
-const tasksContent = document.querySelector('.tasks')
+const inputContent = document.querySelector(".input-container");
+const btnSave = document.querySelector(".button-container");
+const tasksContent = document.querySelector(".tasks");
+const errorMessage = document.querySelector(".errorMessage");
 
-
-
-btnSave.addEventListener('click', addTask);
+btnSave.addEventListener("click", addTask);
 
 function addTask(event) {
-    const inputValue = inputContent.value;
+  const inputValue = inputContent.value;
 
-    if(inputValue.trim() === '') {
-        alert('Preencha uma tarefa por favor!')
-    } else{
-   
-    const newContent = document.createElement('li');
-    newContent.textContent = inputValue;
+  if (inputValue.trim() === "") {
+    errorMessage.textContent = "Por favor, preencha uma tarefa.";
+    inputContent.classList.add("erroTaskExist");
+  } else {
+    errorMessage.textContent = "";
+    inputContent.classList.remove("erroTaskExist");
 
-    tasksContent.appendChild(newContent)   
+    if (taskExists(inputValue.toLowerCase())) {
+      errorMessage.textContent = "Essa tarefa já existe.";
+    } else {
+      const newContent = document.createElement("li");
+      newContent.textContent = inputValue;
+
+      tasksContent.appendChild(newContent);
+    }
+
+    inputContent.value = "";
+  }
+  event.preventDefault();
 }
-    event.preventDefault();   
-}
 
+function taskExists(taskText) {
+  const taskListItems = tasksContent.getElementsByTagName("li");
+  for (let i = 0; i < taskListItems.length; i++) {
+    if (taskListItems[i].textContent.trim().toLowerCase() === taskText) {
+      return true;
+    }
+  }
+  return false;
+}
